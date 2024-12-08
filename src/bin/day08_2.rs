bec9b286929828a6b03492970d9301c2aedcac05
Michael Heasell <michael.heasell@gmail.com>
2024-12-08 18:25:20 +0000

    solution for day 8 part 2

---
+++ src/bin/day08_2.rs
@@ -0,0 +1,116 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs::{self},
+    iter,
+    time::Instant,
+};
+
+use aoc24::{grid::Grid, position::Position};
+
+#[derive(Debug)]
+struct Input {
+    grid: Grid<char>,
+}
+
+fn parse_input(s: &str) -> Input {
+    let lines = s.lines().map(|l| l.to_string()).collect::<Vec<_>>();
+    let grid = Grid::from_strings(&lines);
+    Input { grid }
+}
+
+fn solve(input: &Input) -> usize {
+    let station_groups = {
+        let mut groups = HashMap::<char, Vec<Position>>::new();
+        for (pos, c) in input.grid.enumerate().filter(|(_, c)| **c != '.') {
+            groups
+                .entry(*c)
+                .and_modify(|v| {
+                    v.push(pos);
+                })
+                .or_insert_with(|| vec![pos]);
+        }
+        groups
+    };
+
+    let antinode_locations: HashSet<Position> = station_groups
+        .values()
+        .flat_map(|stations| {
+            gen_antinodes(
+                (input.grid.width as i64, input.grid.height() as i64),
+                stations,
+            )
+        })
+        .filter(|p| input.grid.is_in_bounds(p))
+        .collect();
+
+    antinode_locations.len()
+}
+
+fn gen_antinodes(bounds: (i64, i64), stations: &[Position]) -> impl Iterator<Item = Position> + '_ {
+    let pairs = stations
+        .iter()
+        .enumerate()
+        .flat_map(|(i, p)| stations[i + 1..].iter().map(move |q| (p, q)));
+
+    pairs.flat_map(move |(p, q)| {
+        let dist_x = q.x - p.x;
+        let dist_y = q.y - p.y;
+        gen_in_bounds(bounds, *p, (dist_x, dist_y))
+    })
+}
+
+fn gen_in_bounds(
+    bounds: (i64, i64),
+    p: Position,
+    dist: (i64, i64),
+) -> impl Iterator<Item = Position> {
+    iter::successors(Some(p), move |p| {
+        Some(Position::new(p.x + dist.0, p.y + dist.1))
+    })
+    .take_while(move |p| p.x < bounds.0 && p.y < bounds.1)
+    .chain(
+        iter::successors(Some(p), move |p| {
+            Some(Position::new(p.x - dist.0, p.y - dist.1))
+        })
+        .skip(1)
+        .take_while(|p| p.x >= 0 && p.y >= 0),
+    )
+}
+
+fn main() {
+    let start_time = Instant::now();
+    let input_str = fs::read_to_string("data/day08/input").unwrap();
+    let input = parse_input(&input_str);
+
+    let answer = solve(&input);
+
+    println!("Answer: {}", answer);
+    println!("Elapsed time: {}ms", start_time.elapsed().as_millis());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve1() {
+        let input_str = "\
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+";
+        let input = parse_input(&input_str);
+        let answer = solve(&input);
+
+        assert_eq!(answer, 34);
+    }
+}
