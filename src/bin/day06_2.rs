b64a8655dc89156c97978f7d732ea67550ce5f3a
Michael Heasell <michael.heasell@gmail.com>
2024-12-13 12:53:13 +0000

    move detect_loop to algorithms module
    This function is reusable code from last year.

--- src/bin/day06_2.rs
+++ src/bin/day06_2.rs
@@ -5,7 +5,7 @@ use std::{
     time::Instant,
 };
 
-use aoc24::{direction::Direction, grid::Grid, position::Position};
+use aoc24::{algorithms::detect_loop, direction::Direction, grid::Grid, position::Position};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum TerrainType {
@@ -86,39 +86,6 @@ fn is_valid_obstacle_candidate(
         }
         Some(TerrainType::Blocked) | None => false,
     }
-}
-
-/// Returns (steps before loop, loop length).
-///
-/// Steps before loop is an overestimation.
-/// It is always some multiple of the loop length.
-/// After taking that many steps you are guaranteed
-/// to be inside the loop, but it doesn't tell you
-/// exactly where the loop starts.
-pub fn detect_loop<T, A>(it: &A) -> Option<(usize, usize)>
-where
-    T: Eq,
-    A: Iterator<Item = T> + Clone,
-{
-    let mut a = it.clone();
-    let mut b = it.clone();
-
-    let mut tortoise = a.next()?;
-    b.next()?;
-    let mut hare = b.next()?;
-
-    let mut steps = 1;
-
-    while tortoise != hare {
-        tortoise = a.next()?;
-        b.next()?;
-        hare = b.next()?;
-        steps += 1;
-    }
-
-    let loop_length = a.take_while(|x| *x != hare).count() + 1;
-
-    Some((steps, loop_length))
 }
 
 fn main() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod algorithms;
 pub mod direction;
 pub mod grid;
 pub mod position;
